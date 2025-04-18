341b7ae1e177f964fe13c245c5a9e1247f997085
angelinavt <avtodoro@ucsc.edu>
2025-04-18 12:50:33 -0700

    changes made to lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,12 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 4 Pseudocoding and Problem-solving
+// Author: Angelina Todorov <avtodoro@ucsc.edu>
+// Date:4/18/25
+
+// You and your partner start by placing your fist into your palm.
+    // Then count to three and pound your fist into your palm on every count.
+        // After three say, shoot aloud, and on shoot, pick either a scissors hand position, a fist for a rock, or a resting hand for paper.
+            // Each partner will either have the same or a different position: scissors get broken by rock, paper covers rock, and scissors cut paper.
+            // Play three rounds, and the best out of those rounds wins the game! 
 
 // Constants
 
