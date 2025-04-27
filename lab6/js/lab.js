2ff076b8194905616c5c757ddd10f28f72654d77
angelinavt <avtodoro@ucsc.edu>
2025-04-27 12:09:58 -0700

    changes made

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6 - Arrays and Objects
+// Author: Angelina Todorov
+// Date: 4/27/25
+
+//Define Variables
+myTransport = ["bus", "car", "walking"];
+
+var myMainRide = {
+  make: "Metro Bus",
+  model: "Shuttle Bus",
+  color: "blue and white",
+  year: 2014,
+  age: function() {
+      return 2025 - this.year;
+  }
+}
+
+//output
+document.writeln("Getting around: " + myTransport + "<br>");
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
 
 // Constants
 
