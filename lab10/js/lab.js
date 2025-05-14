b80e500e01f25f409fd9d34d2bd8559541b3217c
angelinavt <avtodoro@ucsc.edu>
2025-05-13 17:41:03 -0700

    changes

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,10 +1,30 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 10 - JavaScript for the Web
+// Author: Angelina Todorov 
+// Date:5/13/25
 
 // Constants
 
 // Functions
+
+function generateRandomText() {
+  const text = "say something.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
+// click listener for button
+$("#make-convo").click(function(){
+});
+
+// get new fake dialogue
+const newText = generateRandomText();
+
+// append a new div to our output div
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
