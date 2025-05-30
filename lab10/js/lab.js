a6097f0be94f6c21620e7859b584742d5217de0a
angelinavt <avtodoro@ucsc.edu>
2025-05-29 17:36:40 -0700

    changes

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -7,25 +7,23 @@
 // Functions
 
 function generateRandomText() {
-  const text = "This is a longer sentence that gives us more random words to play with.";
-  const min = 3;
-  const max = 100;
-  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-  // Get a random starting index to slice the Lorem Ipsum text
-  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-  // Generate the random Lorem Ipsum-like text
-  return text.slice(randStart, randStart + randLen);
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random()*(max - min + 1)) + min;
+    // get a random starting index to splice the lorem ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // generate the random lorem ipsum-like text
+    return text.slice(randStart, randStart + randLen);
 }
+
 // click listener for button
 $("#make-convo").click(function(){
+    // get new fake dialogue
+    const newText = generateRandomText();
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+    console.log("Click");
 });
-
-// get new fake dialogue
-const newText = generateRandomText();
-
-// append a new div to our output div
-$("#output").append('<div class="text"><p>' + newText + '</p></div>');
-
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
